7c8723e0819158ec45c9ab5926a169a80d4803ba
Kiril-Pavlov <Kire_P@yahoo.com>
2022-09-09 22:57:12 +0200

    Cleaned up the project. Added some ideas in comments

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello
+      {/* Header component */}
+      {/* Navbar component Multiple lists and droipdown menus */}
+      {/* Hero carousel component with 3 most sold products */}
+{/* Products page. On click detailed product description */}
+{/* Cart page */}
+{/* Cart component */}
+      {/* Random products section */}
+      {/* Detailed footer component */}
+
+
     </div>
   );
 }
