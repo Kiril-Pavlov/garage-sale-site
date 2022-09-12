7f2156b854e35f727c504e9830f4bdbd93e98696
Kiril-Pavlov <Kire_P@yahoo.com>
2022-09-12 23:03:57 +0200

    added the links in the Navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 import "./Navbar.css"
 
 const Navbar = () => {
+  const linksList = [
+    {linkTo: "/",linkName: "Home"},
+    {linkTo: "/about",linkName: "About"},
+    {linkTo: "/products",linkName: "Products"},
+    {linkTo: "/cartpage",linkName: "Cart"},
+    {linkTo: "/contact",linkName: "Contact"},
+  ];
   return (
-    <div>Navbar</div>
+    <div className='navbar-container'>
+      {linksList.map((item) => {
+        return (
+          <Link to={item.linkTo}>
+            {item.linkName}
+          </Link>
+        )
+      })}
+    </div>
   )
 }
 
