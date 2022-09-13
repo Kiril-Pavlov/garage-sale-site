df4ccc1b1035b30b38d79bf6a2f276a89be0da50
Kiril-Pavlov <Kire_P@yahoo.com>
2022-09-13 23:06:40 +0200

    added some  global css variables and tested them in Navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,18 +5,20 @@ import "./Navbar.css"
 
 const Navbar = () => {
   const linksList = [
-    {linkTo: "/",linkName: "Home"},
-    {linkTo: "/about",linkName: "About"},
-    {linkTo: "/products",linkName: "Products"},
-    {linkTo: "/cartpage",linkName: "Cart"},
-    {linkTo: "/contact",linkName: "Contact"},
+    { linkTo: "/", linkName: "Home" },
+    { linkTo: "/about", linkName: "About" },
+    { linkTo: "/products", linkName: "Products" },
+    { linkTo: "/cartpage", linkName: "Cart" },
+    { linkTo: "/contact", linkName: "Contact" },
   ];
   return (
     <div className='navbar-container'>
       {linksList.map((item) => {
         return (
-          <Link to={item.linkTo}>
-            {item.linkName}
+          <Link to={item.linkTo} className='navbar-link'>
+            <div >
+              {item.linkName}
+            </div>
           </Link>
         )
       })}
