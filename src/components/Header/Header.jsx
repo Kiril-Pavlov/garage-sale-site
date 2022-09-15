a7dd118331ba67a77c88fe095b10980ec2772216
Kiril-Pavlov <Kire_P@yahoo.com>
2022-09-15 22:14:53 +0200

    added structure and styles to header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 
 import "./Header.css"
+import kgsLogo from "../../assets/kgs-logo.png"
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='outer-container'>
+      <div className='header-container'>
+        <div className='header-left'>
+          <img src={kgsLogo} alt="KGS Logo" />
+        </div>
+        <div className='header-right'>
+          <div className='header-language-selection'>
+            <ul>
+              <li>EN</li>
+              <li>MK</li>
+            </ul>
+          </div>
+          <div className='header-search-input'>
+            <input type="text" placeholder='Search'/>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
