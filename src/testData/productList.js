3ff02fb2533699544925979b215cad6a804c6b07
Kiril-Pavlov <Kire_P@yahoo.com>
2022-09-21 22:30:37 +0200

    created some fake data

---
+++ src/testData/productList.js
@@ -0,0 +1,42 @@
+export const productlist = [
+    {
+        id:"1",
+        name:"Toolbox",
+        description:"Toolbox in good shape. Only 3 pisces mising.",
+        img: "urlhere",
+        price: "29",
+        datePosted: "date",
+        promotion: true,
+        tags: ["tools","garage","hardware"]
+    },
+    {
+        id:"2",
+        name:"Guitar",
+        description:"Guitar in playng shape.",
+        img: "urlhere",
+        price: "10.00",
+        datePosted: "date",
+        promotion: false,
+        tags: ["instrument","music"]
+    },
+    {
+        id:"3",
+        name:"Rocking Chair",
+        description:"Wooden rocking chair.",
+        img: "urlhere",
+        price: "19",
+        datePosted: "date",
+        promotion: true,
+        tags: ["furniture","living room","house"]
+    },
+    {
+        id:"4",
+        name:"Vinyl Records",
+        description:"Rock collection of over 50 vinyl records.",
+        img: "urlhere",
+        price: "50",
+        datePosted: "date",
+        promotion: true,
+        tags: ["music"]
+    }
+]
