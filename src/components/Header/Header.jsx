18d7f0ebf21358c1f8e00a93000f650e1885f552
Kiril-Pavlov <Kire_P@yahoo.com>
2022-10-08 21:08:37 +0200

    header-navbar section responsive look done. no logic yet

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 
 import "./Header.css"
 import kgsLogo from "../../assets/kgs-logo.png"
+import {GiHamburgerMenu} from "react-icons/gi"
 
 const Header = () => {
   return (
     <div className='outer-container'>
       <div className='header-container'>
+        <div className='hamburger-menu'>
+          <GiHamburgerMenu fill='#CBB26A'/>
+        </div>
         <div className='header-left'>
           <img src={kgsLogo} alt="KGS Logo" />
         </div>
