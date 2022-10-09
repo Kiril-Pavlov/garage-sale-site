bcc8f3560537522246dbebdf46849894154da43f
Kiril-Pavlov <Kire_P@yahoo.com>
2022-10-09 20:09:00 +0200

    hero section unresponsive done

--- src/pages/Home/components/Hero/Hero.jsx
+++ src/pages/Home/components/Hero/Hero.jsx
@@ -2,9 +2,23 @@ import React from 'react'
 
 import "./Hero.css"
 
+import logoImg from "../../../../assets/kgs-logo.png"
+
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='hero-container'>
+      <div className='hero-logo-left'>
+        <img src={logoImg} alt="Logo" />
+      </div>
+      <div className='hero-content-right'>
+        <h1>Garage Sale site</h1>
+        <h2>Garbage to one. Treasure to another.</h2>
+        <p>Get rid of the things that only take space and don't use them. Also browse for some usefull items in our Products section</p>
+        <div className='hero-button-container'>
+          <button>Browse here ...</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
