8c4cd631df6d73cfe178a82998622581b3801ab4
Kiril-Pavlov <Kire_P@yahoo.com>
2022-10-11 17:55:32 +0200

    About Page component non responsive done

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -2,9 +2,54 @@ import React from 'react'
 
 import "./About.css"
 
+import section1Img from "../../assets/section-one-image.jfif"
+import section2Img from "../../assets/section-two-image.jfif"
+
 const About = () => {
   return (
-    <div>About</div>
+    <div className='about-container'>
+      <h2 className='about-title'>About</h2>
+      <div className='about-content'>
+        <div className='about-section-one'>
+          <div className='section-img'>
+            <img src={section1Img} alt="slika 1" />
+          </div>
+          <div className='section-text'>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui sapiente 
+              voluptatibus fugit labore assumenda porro. Reprehenderit accusantium 
+              iusto ipsum cum aliquam rerum delectus neque? Veniam quod ad tempore 
+              consequatur amet. Ipsa fuga sintnon officiis dignissimos perspiciatis 
+              numquam consequatur cumque?
+            </p>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui sapiente 
+              voluptatibus fugit labore assumenda porro. Reprehenderit accusantium 
+              iusto ipsum cum aliquam rerum delectus neque? Veniam quod ad tempore 
+              consequatur amet. Ipsa fuga sintnon officiis dignissimos perspiciatis 
+              numquam consequatur cumque?
+            </p>
+          </div>
+        </div>
+        <div className='about-section-one'>
+          <div className='section-text'>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui sapiente 
+              voluptatibus fugit labore assumenda porro. Reprehenderit accusantium 
+              iusto ipsum cum aliquam rerum delectus neque? Veniam quod ad tempore 
+              consequatur amet. Ipsa fuga sintnon officiis dignissimos perspiciatis 
+              numquam consequatur cumque?
+            </p>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui sapiente 
+              voluptatibus fugit labore assumenda porro. Reprehenderit accusantium 
+              iusto ipsum cum aliquam rerum delectus neque? Veniam quod ad tempore 
+              consequatur amet. Ipsa fuga sintnon officiis dignissimos perspiciatis 
+              numquam consequatur cumque?
+            </p>
+          </div>
+          <div className='section-img'>
+            <img src={section2Img} alt="slika 2" />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 
 import "./Contact.css"
 
+
+
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+      Contact
+    </div>
   )
 }
 
