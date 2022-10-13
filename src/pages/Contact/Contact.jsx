52a6ab07024a5ab35be646d46d9d671938571a74
Kiril-Pavlov <Kire_P@yahoo.com>
2022-10-13 19:01:22 +0200

    contact form created

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -6,18 +6,26 @@ import "./Contact.css"
 
 const Contact = () => {
   return (
-    <div>
+    <div className='contact-container'>
       <h2>Contact</h2>
-      <div>
+      <div className='form-info-container'>
         <div className='contact-form'>
           <form action="">
-            <label htmlFor="">Enter your name</label>
-            <input type="text" placeholder='ex. Jonh Doe' />
-            <label htmlFor="">Enter your email</label>
-            <input type="text" placeholder='ex. john.doe@gmail.com' />
-            <label htmlFor="">Enter your message here</label>
-            <textarea name="" id="" cols="30" rows="10">Message here ...</textarea>
-            <button>Submit</button>
+            <div className='form-row'>
+              <label htmlFor="">Enter your name</label>
+              <input type="text" placeholder='ex. Jonh Doe' />
+            </div>
+            <div className='form-row'>
+              <label htmlFor="">Enter your email</label>
+              <input type="text" placeholder='ex. john.doe@gmail.com' />
+            </div>
+            <div className='form-textarea'>
+              <label htmlFor="">Enter your message here</label>
+              <textarea name="" id="" cols="30" rows="10">Message here ...</textarea>
+            </div>
+            <div className='form-btn'>
+              <button>Submit</button>
+            </div>
           </form>
         </div>
         <div className='contact-info'>
